50cd0a16df9fabf72f95b5008d286ffd2697a755
Aneesh Pulukkul <aneeshpu@aditi.com>
2013-04-08 14:52:40 +0530

    Adding admin portal web app.

---
+++ AtrioAdminPortalPOC/AtrioAdminPortal/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.IdentityModel.Claims;
+using System.Threading;
+
+namespace AtrioAdminPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string swt = ((ClaimsIdentity)(Thread.CurrentPrincipal.Identity)).BootstrapToken.ToString();
+            // TODO:
+            // 1. Make call to ATRIO MOCK REST API
+            // 2. Pass received permission set to view.
+            // 3. Depending on permission set, decide to enable or disable controls.
+            return View();
+        }
+    }
+}
